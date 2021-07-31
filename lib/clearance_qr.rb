aa85b291c73829d4dc1bc9c04a400330feecaa27
ghennadiimirosnicenco <ghennadiimirosnicenco@charanga.com>
2021-07-31 10:44:40 +0100

    Initial commit

---
+++ lib/clearance_qr.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "clearance_qr/version"
+
+module ClearanceQr
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/clearance_qr_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ClearanceQrTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::ClearanceQr::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
